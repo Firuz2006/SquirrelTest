8a8d84151609a0afce708d0f463f1fa22ecf28cc
Firuz <firuzisoboev@gmail.com>
2025-04-08 15:45:15 +0500

    initialize project with basic configuration and structure

---
+++ src/SquirrelTest/Controllers/VersionController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Squirrel;
+
+namespace SquirrelTest.Controllers;
+
+[Controller]
+[Route("api/[controller]")]
+public class VersionController(IConfiguration configuration) : ControllerBase
+{
+    [HttpGet]
+    public async Task<IActionResult> GetVersion()
+    {
+        var updateUrl = configuration["Squirrel:GithubUpdateUrl"] ??
+                        throw new ArgumentNullException();
+        var updater = await UpdateManager.GitHubUpdateManager(updateUrl);
+        var currentlyInstalledVersion = updater.CurrentlyInstalledVersion();
+
+        return Ok(currentlyInstalledVersion.Version);
+    }
+}
